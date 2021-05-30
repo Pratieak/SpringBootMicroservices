d520e888aa186056d801668d21d95aa5b9bf4712
Prateek Maurya <krprateek25@gmail.com>
2021-05-30 17:25:56 +0530

    CONSERV-0001: Initial Commit

---
+++ consul-service/src/main/java/com/consul/ConsulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulServiceApplication.class, args);
+	}
+
+}
---
+++ consul-service/src/test/java/com/consul/ConsulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
