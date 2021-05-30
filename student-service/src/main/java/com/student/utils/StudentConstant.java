c4738d16845b4f3ceabd4ee2dc93b10bd05defc1
Prateek Maurya <krprateek25@gmail.com>
2021-05-30 16:42:45 +0530

    STUD-0012: Added Constant Class and .gitignore file

---
+++ student-service/src/main/java/com/student/utils/StudentConstant.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.student.utils;
+
+/**
+ * @author Prateek Maurya
+ *
+ */
+
+public class StudentConstant {
+	
+	private StudentConstant() {
+		
+	}
+	public static final String ERROR_DETAILS = "Error Details:= {}";
+	
+}
